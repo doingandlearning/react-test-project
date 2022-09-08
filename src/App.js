06f257fe4958939179b60104aaffe37e8bc93688
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-09-08 18:25:51 +0100

    Added alt text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Testing if the script tag changes.</p>
-        <img src="https://www.fillmurray.com/800/800" />
+        <img
+          alt="Image of Bill Murray"
+          src="https://www.fillmurray.com/800/800"
+        />
       </header>
     </div>
   );
